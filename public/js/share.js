3d2a9cebe13f3ec7abb6fa8cc90e172440cfa4cc
Adarsh <adarshkumarmsay@gmail.com>
2024-10-09 00:51:30 +0530

    Revert "Added social media sharing functionality for blog posts"

--- public/js/share.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Social media share buttons ready!');
-    // Add any future JS code for handling sharing events here
-    // basically if want to expand in future
-  });
-  
