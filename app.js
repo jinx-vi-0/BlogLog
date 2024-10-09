703fff9c138fde1492a91167bd57176466cc2be3
Susritha <susrithasabbini@gmail.com>
2024-10-09 17:11:23 +0530

    Added application logs with morgan

--- app.js
+++ app.js
@@ -1,50 +1,60 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const expressLayout = require('express-ejs-layouts');
-const methodOverride = require('method-override');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
+const express = require("express");
+const expressLayout = require("express-ejs-layouts");
+const methodOverride = require("method-override");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const rfs = require("rotating-file-stream");
 
-const connectDB = require('./server/config/db');
-const { isActiveRoute } = require('./server/helpers/routeHelpers');
+const connectDB = require("./server/config/db");
+const { isActiveRoute } = require("./server/helpers/routeHelpers");
+const morgan = require("morgan");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-  
+
+const accessLogStream = rfs.createStream("application.log", {
+  interval: "1d",
+  path: "./logs",
+});
+
+app.use(morgan("combined", { stream: accessLogStream }));
+
 // Connect to DB
 connectDB();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(methodOverride('_method'));
+app.use(morgan("dev"));
+app.use(methodOverride("_method"));
 
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  store: MongoStore.create({
-    mongoUrl: process.env.MONGODB_URI
-  }),
-  //cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
-}));
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+    }),
+    //cookie: { maxAge: new Date ( Date.now() + (3600000) ) }
+  })
+);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Templating Engine
 app.use(expressLayout);
-app.set('layout', './layouts/main');
-app.set('view engine', 'ejs');
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
 
+app.locals.isActiveRoute = isActiveRoute;
 
-app.locals.isActiveRoute = isActiveRoute; 
+app.use("/", require("./server/routes/main"));
+app.use("/", require("./server/routes/admin"));
 
-
-app.use('/', require('./server/routes/main'));
-app.use('/', require('./server/routes/admin'));
-
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
