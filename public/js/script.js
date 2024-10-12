7135dee4304bcdc4a2a3cbdd6ef72be84183806b
Anshara Beigh <AnsharaBeigh@gmail.com>
2024-10-13 00:38:22 +0530

    Added dark mode toggle, button design changes, and adjusted layout

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,23 @@
 document.addEventListener('DOMContentLoaded', function(){
+  const darkModeToggle = document.getElementById("dark-mode-toggle");
+  const body = document.body;
+  if (darkModeToggle) {
+    darkModeToggle.addEventListener("click", function() {
+        body.classList.toggle("dark-mode");
+        if (body.classList.contains("dark-mode")) {
+          darkModeToggle.textContent = "Light Mode"; // Change button text for clarity
+        } else {
+          darkModeToggle.textContent = "Dark Mode"; // Revert button text
+        }
+    });
+}
 
   const allButtons = document.querySelectorAll('.searchBtn');
   const searchBar = document.querySelector('.searchBar');
   const searchInput = document.getElementById('searchInput');
   const searchClose = document.getElementById('searchClose');
 
+  
   for (var i = 0; i < allButtons.length; i++) {
     allButtons[i].addEventListener('click', function() {
       searchBar.style.visibility = 'visible';
