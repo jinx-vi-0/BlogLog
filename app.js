3bf1fa9a2403a4ce3f35fd12a14f1f440961f8e3
Susritha <106534816+susrithasabbini@users.noreply.github.com>
2024-10-12 23:01:35 +0530

    Add application logs with morgan

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const expressLayout = require('express-ejs-layouts');
-const methodOverride = require('method-override');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
+const express = require("express");
+const expressLayout = require("express-ejs-layouts");
+const methodOverride = require("method-override");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const rfs = require("rotating-file-stream");
 const passport = require('passport'); // Added passport for authentication
 const flash = require('connect-flash'); // Added flash for storing flash messages
 
-const connectDB = require('./server/config/db');
-const { isActiveRoute } = require('./server/helpers/routeHelpers');
+const connectDB = require("./server/config/db");
+const { isActiveRoute } = require("./server/helpers/routeHelpers");
+const morgan = require("morgan");
 
 // Passport configuration
 require('./server/config/passport')(passport); // Passport config to be created separately
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const accessLogStream = rfs.createStream("application.log", {
+  interval: "1d",
+  path: "./logs",
+});
+
+app.use(morgan("combined", { stream: accessLogStream }));
+
+// Connect to DB
 
 app.use(session({
     secret: 'your_secret_key', // Change this to your secret key
@@ -34,18 +45,21 @@ connectDB();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(methodOverride('_method'));
+app.use(morgan("dev"));
+app.use(methodOverride("_method"));
 
 // Session setup for storing user session data
-app.use(session({
-  secret: process.env.SESSION_SECRET || 'keyboard cat', // Use SESSION_SECRET from .env
-  resave: false,
-  saveUninitialized: true,
-  store: MongoStore.create({
-    mongoUrl: process.env.MONGODB_URI // Use MongoDB Atlas URI from .env
-  }),
-  // cookie: { maxAge: new Date(Date.now() + 3600000) } // Uncomment if you want custom cookie expiry time
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "keyboard cat", // Use SESSION_SECRET from .env
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI, // Use MongoDB Atlas URI from .env
+    }),
+    // cookie: { maxAge: new Date(Date.now() + 3600000) }// Uncomment if you want custom cookie expiry time
+  })
+);
 
 // Passport middleware
 app.use(passport.initialize());
@@ -61,7 +75,7 @@ app.use((req, res, next) => {
 });
 
 // Static files
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Templating Engine
 app.use(expressLayout);
