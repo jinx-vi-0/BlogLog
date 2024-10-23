a4d5abcf377332e6be9b656d2b1aa04083a00f78
Anshara Beigh <AnsharaBeigh@gmail.com>
2024-10-23 12:14:56 +0530

    Your commit message

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,26 @@
 document.addEventListener('DOMContentLoaded', function(){
   const darkModeToggle = document.getElementById("dark-mode-toggle");
   const body = document.body;
+
+  const savedDarkMode = localStorage.getItem('darkMode');
+  if (savedDarkMode == 'enabled') {
+    body.classList.add('dark-mode');
+  } else {
+    body.classList.remove('dark-mode');
+    if (darkModeToggle) darkModeToggle.textContent ='Dark Mode';
+  }
+  
+
   if (darkModeToggle) {
     darkModeToggle.addEventListener("click", function() {
         body.classList.toggle("dark-mode");
-        if (body.classList.contains("dark-mode")) {
-          darkModeToggle.textContent = "Light Mode"; // Change button text for clarity
+
+        if (body.classList.contains('dark-mode')){
+          localStorage.setItem('darkMode','enabled');
+          darkModeToggle.textContent ="Light Mode";
         } else {
-          darkModeToggle.textContent = "Dark Mode"; // Revert button text
+          localStorage.setItem('darkMode','disabled');
+          darkModeToggle.textContent ="Dark Mode";
         }
     });
 }
